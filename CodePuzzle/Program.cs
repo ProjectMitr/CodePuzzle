5e3c101fdcd34be562bec4c8306cf88c7e361062
avi <mitaars@gmail.com>
2016-08-04 21:39:31 +0530

    commit1

--- CodePuzzle/Program.cs
+++ CodePuzzle/Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CodePuzzle
 {
     class Program
     {
+        static void expression()
+        { 
+        string x= "my world";
+        string y = "kabali";
+        x = x + y;
+        Console.WriteLine(x);
+        Console.ReadLine();
+        }
+
+        static void expression1()
+        {
+            string x = "my world";
+            string y = "ram";
+            x += y;
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
+            expression();
         }
     }
 }
